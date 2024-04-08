e8d5f0335c8baf6d7ba3c233754c8a38ff4bcc3a
Sharifjon <sharifboymuminov@gmail.com>
2024-04-08 15:02:05 +0500

    ...

---
+++ lib/screens/category/category_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Category_Screen extends StatefulWidget {
+  const Category_Screen({super.key});
+
+  @override
+  State<Category_Screen> createState() => _Category_ScreenState();
+}
+
+class _Category_ScreenState extends State<Category_Screen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
---
+++ lib/screens/produc_screen/produc_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Product_Screen extends StatefulWidget {
+  const Product_Screen({super.key});
+
+  @override
+  State<Product_Screen> createState() => _Product_ScreenState();
+}
+
+class _Product_ScreenState extends State<Product_Screen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
