844f4970ed6be174265dc39f7e6799ae4cd1d77f
Sharifjon <sharifboymuminov@gmail.com>
2024-04-19 21:07:21 +0500

    ...

---
+++ lib/screens/category/add_category_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class AddCategoryScreen extends StatefulWidget {
+  const AddCategoryScreen({super.key});
+
+  @override
+  State<AddCategoryScreen> createState() => _AddCategoryScreenState();
+}
+
+class _AddCategoryScreenState extends State<AddCategoryScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+    );
+  }
+}
