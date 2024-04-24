04a5015691a342d5adc83768fb8c564be9726df6
Sharifjon <sharifboymuminov@gmail.com>
2024-04-24 18:02:29 +0500

    Makkami?

--- lib/screens/category/category_screen.dart
+++ lib/screens/category/category_screen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:gavhar_app/blocs/category/category_bloc.dart';
+import 'package:gavhar_app/blocs/category/category_state.dart';
 import 'package:gavhar_app/data/local/local_varibals.dart';
-import 'package:gavhar_app/data/models/category/category_model.dart';
 import 'package:gavhar_app/screens/category/add_category_screen.dart';
 import 'package:gavhar_app/screens/produc/widget/stagger_mygrid.dart';
 import 'package:gavhar_app/utils/app_colors.dart';
@@ -52,66 +54,65 @@ class _CategoryScreenState extends State<CategoryScreen> {
           SizedBox(width: 10.we),
         ],
       ),
-      body: Stack(
-        children: [
-          // const BackGroundItem(
-          //   colorOne: Colors.redAccent,
-          // ),
-          StaggerGridMyWidget(
-            child: List.generate(20, (index) {
-              return Container(
-                padding: EdgeInsets.symmetric(vertical: 10.he),
-                alignment: Alignment.bottomCenter,
-                height: index.isEven ? 200.he : 250.he,
-                decoration: BoxDecoration(
-                  color: AppColors.c_FFFFFF,
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.grey.withOpacity(0.27),
-                      blurRadius: 30,
-                      spreadRadius: 1,
-                      offset: const Offset(0, 17),
-                    ),
-                  ],
-                  borderRadius: BorderRadius.circular(10.r),
-                  image: DecorationImage(
-                    image: NetworkImage(categoryModel.imageUrl),
-                    fit: BoxFit.cover,
-                  ),
-                ),
-                child: Text(
-                  categoryModel.categoryName,
-                  maxLines: 2,
-                  textAlign: TextAlign.center,
-                  overflow: TextOverflow.ellipsis,
-                  style: TextStyle(
-                    color: AppColors.c_FFFFFF,
-                    fontWeight: FontWeight.w700,
-                    fontSize: 20.sp,
-                    shadows: [
-                      BoxShadow(
-                        color: Colors.amber.withOpacity(0.7),
-                        blurRadius: 10,
-                        spreadRadius: 10,
-                        offset: const Offset(0, 0),
+      body: BlocBuilder<CategoryBloc, CategoryState>(
+        builder: (BuildContext context, CategoryState state) {
+          if (state is ErrorCategoryState) {
+            return Center(child: Text(state.errorText));
+          }
+          if (state is SuccessCategoryState) {
+            return Stack(
+              children: [
+                StaggerGridMyWidget(
+                  child: List.generate(20, (index) {
+                    return Container(
+                      padding: EdgeInsets.symmetric(vertical: 10.he),
+                      alignment: Alignment.bottomCenter,
+                      height: index.isEven ? 200.he : 250.he,
+                      decoration: BoxDecoration(
+                        color: AppColors.c_FFFFFF,
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey.withOpacity(0.27),
+                            blurRadius: 30,
+                            spreadRadius: 1,
+                            offset: const Offset(0, 17),
+                          ),
+                        ],
+                        borderRadius: BorderRadius.circular(10.r),
+                        image: DecorationImage(
+                          image: NetworkImage(state.categories[index].imageUrl),
+                          fit: BoxFit.cover,
+                        ),
                       ),
-                    ],
-                  ),
+                      child: Text(
+                        state.categories[index].categoryName,
+                        maxLines: 2,
+                        textAlign: TextAlign.center,
+                        overflow: TextOverflow.ellipsis,
+                        style: TextStyle(
+                          color: AppColors.c_FFFFFF,
+                          fontWeight: FontWeight.w700,
+                          fontSize: 20.sp,
+                          shadows: [
+                            BoxShadow(
+                              color: Colors.amber.withOpacity(0.7),
+                              blurRadius: 10,
+                              spreadRadius: 10,
+                              offset: const Offset(0, 0),
+                            ),
+                          ],
+                        ),
+                      ),
+                    );
+                  }),
                 ),
-              );
-            }),
-          ),
-        ],
+              ],
+            );
+          }
+
+          return const Center(child: CircularProgressIndicator.adaptive());
+        },
       ),
     );
   }
 }
-
-CategoryModel categoryModel = CategoryModel(
-  storagePath: "as",
-  countProduct: 0,
-  categoryName: "Uzuklar",
-  docId: "",
-  imageUrl:
-      "https://www.thomassabo.com/dw/image/v2/AAQY_PRD/on/demandware.static/-/Library-Sites-global/default/dw1e598834/00024_PD/startseite/08.03.2024/LP-Home-Heroteaser-Desktop-Sterling-Pink-Trendfarbe.jpg?sfrm=png",
-);
