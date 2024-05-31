73022717563725a3acbc48fa9553c50607e0284b
Sharifjon <sharifboymuminov@gmail.com>
2024-05-31 23:02:30 +0500

    :)

--- lib/screens/splash/splash_screen.dart
+++ lib/screens/splash/splash_screen.dart
@@ -19,7 +19,7 @@ class _SplashScreenState extends State<SplashScreen> {
 
   nextHome() async {
     Future.delayed(
-      const Duration(seconds: 1),
+      const Duration(seconds: 3),
       () {
         Navigator.pushReplacement(
           context,
